dc44944a2e642281c26369a334595fac5677a229
Jan <125610144+Jongjan88@users.noreply.github.com>
2024-01-19 21:45:50 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -19,6 +19,9 @@
 #include <stdlib.h>
 #include <sys/statvfs.h>
 #include <cstdlib>
+#include "stor.h"
+
+// disk
 typedef struct DiskSpaceInfo
 {
   unsigned long long total_space;
@@ -26,52 +29,6 @@ typedef struct DiskSpaceInfo
   unsigned long long used_space;
 } DiskSpaceInfo;
 // disk
-
-// Int System
-int connection = 0;
-int sa_sys_cpu = 0;
-int sa_sys_rss = 0;
-int sa_sys_vsz = 0;
-int sa_sys_ram_used = 0;
-int sa_sys_ram_total = 0;
-
-// Mainnet
-int rpc_main_con = 0;
-int m_version = 0;
-int m_connections = 0;
-int m_transactions = 0;
-int m_blocks = 0;
-int m_headers = 0;
-int m_uptime = 0;
-int m_set_uptime = 0;
-
-// Testnet
-int rpc_test_con = 0;
-int t_version = 0;
-int t_connections = 0;
-int t_transactions = 0;
-int t_blocks = 0;
-int t_headers = 0;
-int t_uptime = 0;
-int t_set_uptime = 0;
-
-// Char
-char *c_dgbn_api;
-char *cm_enabled;
-char *cm_rpc_ip;
-char *cm_rpc_port;
-char *cm_rpc_user;
-char *cm_rpc_password;
-char *cm_output;
-char *ct_enabled;
-char *ct_rpc_ip;
-char *ct_rpc_port;
-char *ct_rpc_user;
-char *ct_rpc_password;
-char *ct_output;
-char api_url[] = "https://digibytenode.com/api";
-
-volatile int run = 1;
 
 size_t WriteCallback(void *contents, size_t size, size_t nmemb, std::string *output)
 {
