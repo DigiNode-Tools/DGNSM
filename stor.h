413ed6d85f16dd75d4231f2193296addcd5cb570
Jan <125610144+Jongjan88@users.noreply.github.com>
2024-01-19 21:44:29 +0100

    Create stor.h

---
+++ stor.h
@@ -0,0 +1,50 @@
+#ifndef STOR_H
+#define STOR_H
+
+// Int System
+int connection = 0;
+int sa_sys_cpu = 0;
+int sa_sys_rss = 0;
+int sa_sys_vsz = 0;
+int sa_sys_ram_used = 0;
+int sa_sys_ram_total = 0;
+
+// Mainnet
+int rpc_main_con = 0;
+int m_version = 0;
+int m_connections = 0;
+int m_transactions = 0;
+int m_blocks = 0;
+int m_headers = 0;
+int m_uptime = 0;
+int m_set_uptime = 0;
+
+// Testnet
+int rpc_test_con = 0;
+int t_version = 0;
+int t_connections = 0;
+int t_transactions = 0;
+int t_blocks = 0;
+int t_headers = 0;
+int t_uptime = 0;
+int t_set_uptime = 0;
+
+// Char
+char *c_dgbn_api;
+char *cm_enabled;
+char *cm_rpc_ip;
+char *cm_rpc_port;
+char *cm_rpc_user;
+char *cm_rpc_password;
+char *cm_output;
+char *ct_enabled;
+char *ct_rpc_ip;
+char *ct_rpc_port;
+char *ct_rpc_user;
+char *ct_rpc_password;
+char *ct_output;
+char api_url[] = "https://digibytenode.com/api";
+
+volatile int run = 1;
+
+#endif // STOR_H
